54c71057ea0648903febd7eb7d83d9e965eefd3f
ayberkozcan <ozcanayberk249@gmail.com>
2025-03-09 13:49:18 +0300

    main.py

--- src/prepare_data.py
+++ src/prepare_data.py
@@ -1,7 +1,9 @@
+import os
+
 from torchvision import datasets, transforms
 from torch.utils.data import DataLoader
 
-def prepare_data(root, split, transform, batch_size):
+def prepare_data(train_dir, test_dir):
     transform = transforms.Compose([
         transforms.Resize((423, 640)),
         transforms.ToTensor(),
@@ -9,29 +11,36 @@ def prepare_data(root, split, transform, batch_size):
     ])
 
     train_data = datasets.OxfordIIITPet(
-        root,
-        split,
-        transform=transform
+        train_dir,
+        split="trainval",
+        transform=transform,
+        download=True
     )
+
     test_data = datasets.OxfordIIITPet(
-        root,
-        split,
-        transform=transform
+        test_dir,
+        split="test",
+        transform=transform,
+        download=True
     )
+
+    print("\n[INFO] Dataset is ready\n")
 
     class_names = train_data.classes
 
     train_dataloader = DataLoader(
         train_data,
-        batch_size=batch_size,
+        batch_size=32,
         shuffle=True,
         num_workers=1,
     )
     test_dataloader = DataLoader(
         test_data,
-        batch_size=batch_size,
+        batch_size=32,
         shuffle=False,
         num_workers=1
     )
+    
+    print("\n[INFO] Dataloaders are ready\n")
 
     return train_dataloader, test_dataloader, class_names
